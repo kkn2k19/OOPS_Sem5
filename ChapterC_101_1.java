0bc226e7af4cf0be972b3838e67e1d53433137cf
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-07-26 15:56:09 +0530

    Create ChapterC_101_1.java

---
+++ ChapterC_101_1.java
@@ -0,0 +1,26 @@
+// 1.
+
+package chapterc_101_1;
+
+public class ChapterC_101_1 {
+    public static void main(String[] args) {
+        Difference obj = new Difference();
+        obj.set(101, "Karan");
+        obj.display();
+    }
+}
+
+class Difference {
+    int roll = 100;
+    String name = "Rahul";
+    
+    void set(int roll, String name){
+        this.roll=roll;
+        this.name = name;
+    }
+    
+    void display(){
+        System.out.println("Name : " + name);
+        System.out.println("Roll No. : " + roll);
+    }
+}
