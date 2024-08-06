f454f9eced853c73e399be279e6b8a45b497dd0d
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-08-06 16:26:40 +0530

    Update ChapterD_101_5.java

--- ChapterD/ChapterD_101_5.java
+++ ChapterD/ChapterD_101_5.java
@@ -1,19 +1,17 @@
-#NOT COMPLETED YET !!!!!
-
 // 5. Write a class with the name Perimeter using function overloading that computes
 // the perimeter of a square, a rectangle, and a circle.
 // The perimeter of a square = 4 * s
 // The perimeter of a rectangle = 2 * (l + b)
 // The perimeter of a circle = 2 * (22/7) * r
 
-// package chapterd_101_5;
+ package chapterd_101_5;
 
 public class ChapterD_101_5 {
     public static void main(String[] args) {
         Perimeter obj = new Perimeter();
 
-        double squarePerimeter = obj.perimeter(5.0);
-        System.out.println("Perimeter of Square with side 5.0 : " + squarePerimeter);
+        double squarePerimeter = obj.perimeter(5);
+        System.out.println("Perimeter of Square with side 5 : " + squarePerimeter);
 
         double rectanglePerimeter = obj.perimeter(10.0, 5.0);
         System.out.println("Perimeter of Rectangle with length 10.0 and breadth 5.0 : " + rectanglePerimeter);
@@ -24,7 +22,7 @@ public class ChapterD_101_5 {
 }
 
 class Perimeter {
-    double perimeter(double s) {
+    double perimeter(int s) {
         return (4 * s);
     }
 
@@ -36,3 +34,12 @@ class Perimeter {
         return (2 * (22.0/7.0) * r);
     }
 }
+
+
+/*
+OUTPUT -- 
+
+Perimeter of Square with side 5 : 20.0
+Perimeter of Rectangle with length 10.0 and breadth 5.0 : 30.0
+Perimeter of Circle with radius 4.0 : 25.142857142857142
+*/
